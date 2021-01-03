ed2a2f79e8ccc7c846d70938413118c9e89dd508
Craig <craigmiller160@gmail.com>
2021-01-03 15:20:17 -0500

    Began working on more methods

--- src/methods/fpDelete.ts
+++ src/methods/fpDelete.ts
@@ -3,9 +3,9 @@ import { AjaxApi, UriRequestConfig } from '@craigmiller160/ajax-api';
 import { AxiosResponse } from 'axios';
 import { handleTEReason } from '../utils/handleTEReason';
 
-export const fpGet = (api: AjaxApi) =>
+export const fpDelete = (api: AjaxApi) =>
     <R>(req: UriRequestConfig): TE.TaskEither<Error, AxiosResponse<R>> =>
         TE.tryCatch<Error,AxiosResponse<R>>(
-            () => api.get<R>(req),
+            () => api.delete<R>(req),
             (reason: unknown) => handleTEReason(reason)
         );
