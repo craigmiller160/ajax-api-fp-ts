a208f169d6306d6e50c26198e47a2e56c1ee937c
Craig <craigmiller160@gmail.com>
2021-01-03 15:30:43 -0500

    Implemented more methods

---
+++ src/methods/fpPost.ts
@@ -0,0 +1,11 @@
+import * as TE from 'fp-ts/es6/TaskEither';
+import { AjaxApi, UriBodyRequestConfig } from '@craigmiller160/ajax-api';
+import { AxiosResponse } from 'axios';
+import { handleTEReason } from '../utils/handleTEReason';
+
+export const fpPost = (api: AjaxApi) =>
+    <B,R>(req: UriBodyRequestConfig<B>): TE.TaskEither<Error, AxiosResponse<R>> =>
+        TE.tryCatch<Error,AxiosResponse<R>>(
+            () => api.post<B,R>(req),
+            (reason: unknown) => handleTEReason(reason)
+        );
