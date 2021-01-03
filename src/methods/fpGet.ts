c461327d30dcd1b75cfcfd31cb9826070385ae93
Craig <craigmiller160@gmail.com>
2021-01-03 15:13:55 -0500

    Integrated handling of TE reason

--- src/methods/fpGet.ts
+++ src/methods/fpGet.ts
@@ -1,9 +1,10 @@
 import * as TE from 'fp-ts/es6/TaskEither';
 import { AjaxApi, UriRequestConfig } from '@craigmiller160/ajax-api';
 import { AxiosResponse } from 'axios';
+import { handleTEReason } from '../utils/handleTEReason';
 
 export const fpGet = <R>(api: AjaxApi) =>
     (req: UriRequestConfig) => TE.tryCatch<Error,AxiosResponse<R>>(
         () => api.get<R>(req),
-        (reason: unknown) => new Error()
+        (reason: unknown) => handleTEReason(reason)
     );
---
+++ src/utils/handleTEReason.ts
@@ -0,0 +1,7 @@
+
+export const handleTEReason = (reason: unknown): Error => {
+    if (reason instanceof Error) {
+        return reason;
+    }
+    return new Error(`${reason}`);
+};
