496596b0ab7e0c74ab4ee4c4e171b531e6dbdfa0
Craig <craigmiller160@gmail.com>
2021-01-03 15:32:16 -0500

    Integrated more methods into the API

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { ApiConfig, createApi as createBaseApi } from '@craigmiller160/ajax-api';
 import { fpGet } from './methods/fpGet';
 import { FpAjaxApi } from './types';
+import { fpPost } from './methods/fpPost';
+import { fpPut } from './methods/fpPut';
+import { fpDelete } from './methods/fpDelete';
 
 export const createApi = (config?: ApiConfig): FpAjaxApi => {
     const api = createBaseApi(config);
     return {
         instance: api.instance,
-        get: fpGet(api)
-    }
+        get: fpGet(api),
+        post: fpPost(api),
+        put: fpPut(api),
+        delete: fpDelete(api)
+    };
 };
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,11 @@
 import { AxiosInstance, AxiosResponse } from 'axios';
-import { UriRequestConfig } from '@craigmiller160/ajax-api';
+import { UriBodyRequestConfig, UriRequestConfig } from '@craigmiller160/ajax-api';
 import { TaskEither } from 'fp-ts/es6/TaskEither';
 
 export interface FpAjaxApi {
     instance: AxiosInstance;
     get: <R>(req: UriRequestConfig) => TaskEither<Error, AxiosResponse<R>>;
+    post: <B,R>(req: UriBodyRequestConfig<B>) => TaskEither<Error, AxiosResponse<R>>;
+    put: <B,R>(req: UriBodyRequestConfig<B>) => TaskEither<Error, AxiosResponse<R>>;
+    delete: <R>(req: UriRequestConfig) => TaskEither<Error, AxiosResponse<R>>;
 }
