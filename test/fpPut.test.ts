197859d799ab3d5d966d662d9e922ae15e0579d1
Craig <craigmiller160@gmail.com>
2021-01-03 17:11:14 -0500

    Implemented more tests

--- test/fpPut.test.ts
+++ test/fpPut.test.ts
@@ -5,13 +5,41 @@ import * as TE from 'fp-ts/es6/TaskEither';
 import { AxiosError } from 'axios';
 
 const uri = '/foo/bar';
+const body = {
+    hello: 'world'
+};
+type BodyType = typeof body;
 
 describe('fpPut', () => {
     it('request success', () => {
-        throw new Error();
+        const api = createApi();
+        const mockApi = new MockAdapter(api.instance);
+        mockApi.onPut(uri, body)
+            .reply(200, 'Success');
+
+        return pipe(
+            api.put<BodyType,string>({
+                uri,
+                body
+            }),
+            TE.map((res) => expect(res.data).toEqual('Success')),
+            TE.mapLeft((ex) => expect(ex).toBeUndefined())
+        )();
     });
 
     it('request error', () => {
-        throw new Error();
+        const api = createApi();
+        new MockAdapter(api.instance);
+
+        return pipe(
+            api.put<BodyType,string>({
+                uri,
+                body
+            }),
+            TE.map((res) => expect(res).toBeUndefined()),
+            TE.mapLeft((ex: Error) => {
+                expect((ex as AxiosError).response?.status).toEqual(404);
+            })
+        )();
     });
 });
