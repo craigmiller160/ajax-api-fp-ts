ba8ee43a4a85a33f330bbac9aa7c288efe05e917
Craig <craigmiller160@gmail.com>
2021-01-03 15:17:19 -0500

    Type improvements

--- src/methods/fpGet.ts
+++ src/methods/fpGet.ts
@@ -2,9 +2,10 @@ import * as TE from 'fp-ts/es6/TaskEither';
 import { AjaxApi, UriRequestConfig } from '@craigmiller160/ajax-api';
 import { AxiosResponse } from 'axios';
 import { handleTEReason } from '../utils/handleTEReason';
+import { TaskEither } from 'fp-ts/es6/TaskEither';
 
 export const fpGet = (api: AjaxApi) =>
-    <R>(req: UriRequestConfig) =>
+    <R>(req: UriRequestConfig): TaskEither<Error, AxiosResponse<R>> =>
         TE.tryCatch<Error,AxiosResponse<R>>(
             () => api.get<R>(req),
             (reason: unknown) => handleTEReason(reason)
